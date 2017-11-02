19e7fab139ebcbff67606988ea75dcd6e66736ab
WALIII <bliberti@bu.edu>
2017-11-01 20:42:49 -0700

    remove extra repos

--- FS_Data.m
+++ FS_Data.m
@@ -12,13 +12,13 @@ range = align-mm:align+mp;
 disp('Formatting data')
 for cell = 1:size(calcium,2);
   for trial = 1:size(calcium{cell},1);
-      if Motif_ind(3,trial) == 0 %&& Motif_ind(1,trial) == 3;
+      if Motif_ind(3,trial) == 0 %&& Motif_ind(1,trial) == 1;
         % temp = detrend(smooth(calcium{cell}(trial,range)));
         temp = (tsmovavg(calcium{cell}(trial,range),'s',Smh)); temp(:,1:Smh) = []; temp = detrend(temp);
        
         data.undirected(triala,:,cell) = (temp(:,1:end));
         triala = triala+1;
-      elseif Motif_ind(3,trial) == 1 %&& Motif_ind(1,trial) == 3;
+      elseif Motif_ind(3,trial) == 1 %&& Motif_ind(1,trial) == 1;
        %  temp = detrend(smooth(calcium{cell}(trial,range)));
        temp = (tsmovavg(calcium{cell}(trial,range),'s',Smh)); temp(:,1:Smh) = []; temp = detrend(temp);
         data.directed(trialb,:,cell) = (temp(:,1:end));
